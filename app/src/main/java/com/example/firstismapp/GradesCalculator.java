635b99802a324d97fdbb32d42e676aba304038e0
Jakub <wetido@gmail.com>
2020-03-25 08:44:35 +0100

    Scrollview, showing name and surname in output

--- app/src/main/java/com/example/firstismapp/GradesCalculator.java
+++ app/src/main/java/com/example/firstismapp/GradesCalculator.java
@@ -3,17 +3,28 @@ package com.example.firstismapp;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+import static java.lang.String.valueOf;
 
 public class GradesCalculator extends AppCompatActivity {
 
-    TextView gradesCos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,26 +32,86 @@ public class GradesCalculator extends AppCompatActivity {
         setContentView(R.layout.activity_grades_calculator);
 
         Intent intent = getIntent();
-        String grades = intent.getStringExtra("grades");
+        final String grades = intent.getStringExtra("grades");
+        final String name = intent.getStringExtra( "name");
+        final String surName = intent.getStringExtra("surName");
 
-        gradesCos = (TextView)findViewById(R.id.gradesNumberA2);
-        gradesCos.setText(grades);
+        final LinearLayout ll = (LinearLayout)findViewById(R.id.linearLayoutMain);
+        final Integer intGrades = Integer.parseInt(grades);
 
-        LinearLayout ll = (LinearLayout)findViewById(R.id.layout);
+        for (int i = 0; i < intGrades; i++) {
 
+            TextView text = new TextView( GradesCalculator.this );
+            text.setId(i + intGrades);
+            text.setBackgroundColor(Color.rgb(70, 80, 90));
 
-        for (int i = 1; i <= Integer.parseInt(grades); i++) {
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT);
-            Button btn = new Button(this);
-            btn.setId(i);
-            final int id_ = btn.getId();
-            btn.setText("button " + id_);
-            btn.setBackgroundColor(Color.rgb(70, 80, 90));
-            ll.addView(btn, params);
+            SeekBar seekBar = new SeekBar(this);
+            seekBar.setId(i);
+            seekBar.setMax(6);
+            seekBar.setProgress(0);
+            seekBar.setBackgroundColor(Color.rgb(65, 75, 85));
+
+            seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) { }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) { }
+
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) {
+
+                    final Integer id = seekBar.getId();
+                    final TextView text2 = findViewById(id + intGrades);
+                    text2.setText( valueOf(progress) );
+                }
+            });
+
+            ll.addView(seekBar);
+            ll.addView(text);
 
         }
+
+        Button button = new Button(this);
+        button.setText("Oblicz");
+        button.setBackgroundColor(Color.rgb(80,200,200));
+        ll.addView(button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                Double avg;
+                Integer count = 0;
+                Boolean validate = true;
+
+                for (int i = 0; i < Integer.parseInt(grades); i++) {
+
+                    SeekBar seekBar = (SeekBar)findViewById(i);
+                    if(seekBar.getProgress() == 0){
+                        validate = false;
+                        break;
+                    }
+                    count += seekBar.getProgress();
+                }
+
+                if( validate ){
+
+                    avg = count/Double.parseDouble(grades);
+
+                    Intent resultIntent = new Intent();
+                    resultIntent.putExtra("result", avg);
+                    resultIntent.putExtra("result2", name + " " + surName);
+                    setResult(RESULT_OK, resultIntent);
+                    finish();
+
+                } else {
+
+                    Toast.makeText(GradesCalculator.this, "WARTOSC NIE MOZE BYC 0", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/firstismapp/MainActivity.java
+++ app/src/main/java/com/example/firstismapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.firstismapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,7 +8,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.regex.Pattern;
 
@@ -15,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     public static final int CALCULATE=3;
-    private static final Pattern USERNAME_PATTERN = Pattern.compile("[a-zA-Z0-9]{1,250}");
+    private static final Pattern USERNAME_PATTERN = Pattern.compile("[A-Z]{1}+[a-zA-Z0-9]{2,250}");
     private static final Pattern GRADES_PATERN = Pattern.compile("^[0-9]*$");
 
     Button calculate;
@@ -33,16 +37,6 @@ public class MainActivity extends AppCompatActivity {
         usSurName = (EditText)findViewById(R.id.surNameInput);
         usGrades = (EditText)findViewById(R.id.gradesNumberInput);
 
-        /*
-        ((Button)findViewById(R.id.caculateActivityButton)).setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent calculateIntent=new Intent(MainActivity.this, GradesCalculator.class);
-                        startActivityForResult(circleIntent, CALCULATE);
-                    }
-                }
-        );  */
 
         calculate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,17 +49,37 @@ public class MainActivity extends AppCompatActivity {
 
                     Toast.makeText(MainActivity.this, "DANE NIE MOGA BYC PUSTE",Toast.LENGTH_LONG).show();
 
-                } else if (!CheckUsername(userName) || !CheckUsername(userSurName) || !CheckGrades(userGrades)){
+                } else if (!CheckUsername(userName) || !CheckUsername(userSurName) || !CheckGrades(userGrades)) {
                     Toast.makeText(MainActivity.this, "PODAJ PRAWIDŁOWE DANE", Toast.LENGTH_LONG).show();
+                }else if (Integer.parseInt( userGrades ) > 50){
 
+                    Toast.makeText(MainActivity.this, "MAKSYMALNA LICZBA OCEN - 50", Toast.LENGTH_LONG).show();
                 }else{
                     Intent i = new Intent(getApplicationContext(), GradesCalculator.class);
                     i.putExtra("grades", userGrades );
-                    startActivity(i);
+                    i.putExtra("name", userName );
+                    i.putExtra("surName", userSurName );
+                    startActivityForResult(i,1);
                 }
             }
         });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1){
+            if(resultCode == RESULT_OK){
+
+                double gradeAVG = data.getDoubleExtra("result", 0);
+                String fullName = data.getStringExtra("result2");
+
+                TextView gradeResult = findViewById(R.id.avgGrade);
+                gradeResult.setText( "Witaj " + fullName + " Twoja średnia ocen to " + String.valueOf( gradeAVG ) );
+            }
+        }
     }
 
     private boolean CheckUsername(String username) {
